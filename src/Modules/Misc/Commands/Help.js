ddf9c75e0cae354dd796e8eb378c67e043430fcf
Mafia-boss-lvl-420 <67177667+Mafia-boss-lvl-420@users.noreply.github.com>
2020-11-23 06:38:02 -0500

    Update Help.js

--- src/Modules/Misc/Commands/Help.js
+++ src/Modules/Misc/Commands/Help.js
@@ -11,24 +11,3 @@ module.exports = class Help extends baseCmd {
         msg.channel.send("hello")
     }
 }
-/*
-module.exports = class{
-    constructor(bot){
-        this.bot = bot;
-
-        this.name = "help";
-        this.alli = [];
-
-        this.dms = true;
-
-        this.helpEmbed = {
-            title: 'Help command'
-        }
-    }
-
-    async run(msg){
-        msg.channel.send("hello")
-    }
-
-}
-*/
