b1daf6cf0c2867f516bb0f922b91b4022bd7538f
Mafia-boss-lvl-420 <67177667+Mafia-boss-lvl-420@users.noreply.github.com>
2020-11-25 09:16:40 -0500

    added a github field on links cmd

--- src/Constants/Config.js
+++ src/Constants/Config.js
@@ -2,5 +2,6 @@ module.exports = {
     defaultPrefix: ".",
     onceFiles: ["ready.js"],
     supportServer: 'https://discord.gg/tqeyYrS43A',
-    inviteLink: 'https://indev.dev'
+    inviteLink: 'https://indev.dev',
+    githubLink: 'https://github.com/monke-dev/Discord-bot'
 }
--- src/Modules/Misc/Commands/Links.js
+++ src/Modules/Misc/Commands/Links.js
@@ -24,6 +24,7 @@ module.exports = class Help extends baseCmd {
 
         embed.fields.push({name: 'Support server', value: `[Here](${config.supportServer})`, inline: true});
         embed.fields.push({name: `Invite link`, value: `[Here](${config.inviteLink})`, inline: true});
+        embed.fields.push({name: 'GitHub repository', value: `[Here](${config.githubLink})`, inline: true})
 
         msg.channel.send({embed: embed});
     
